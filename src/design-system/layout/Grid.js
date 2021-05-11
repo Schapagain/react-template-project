67a35d5b101af73ca50f2264a202d5ebfeadd8b6
Sandesh <chapagainsandesh88@gmail.com>
2021-05-11 18:57:48 -0400

    setup loading

--- src/design-system/layout/Grid.js
+++ src/design-system/layout/Grid.js
@@ -1,9 +1,10 @@
 import React from "react";
+import LoadingDots from "../loading/LoadingDots";
 
-export default function Grid({ children }) {
+export default function Grid({ isLoading, children }) {
   return (
-    <div className="grid items-center justify-center gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      {children}
+    <div className="grid relative items-center justify-center gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      {isLoading ? <LoadingDots /> : children}
     </div>
   );
 }
--- src/design-system/loading/LoadingDots.js
+++ src/design-system/loading/LoadingDots.js
@@ -4,20 +4,18 @@ import classNames from "classnames";
 export default function LoadingDots({ color = "theme" }) {
   const dot = classNames({
     "w-2 h-2 rounded-full animate-loading-dot": true,
-    [`bg-${color}`]: true,
+    [`bg-white`]: true,
   });
-  const dot1 = classNames(dot, {});
-  const dot2 = classNames(dot, {
-    "delay-333": true,
-  });
-  const dot3 = classNames(dot, { "delay-667": true });
 
   return (
-    <div className="my-auto justify-center mx-1 h-fit-content flex appearance-none">
+    <div
+      className={`absolute bg-${color} inset-0 justify-center mx-1 h-fit-content flex appearance-none`}
+      aria-hidden="true"
+    >
       <div className="flex my-auto justify-between w-9">
-        <span aria-hidden="true" className={dot1} />
-        <span aria-hidden="true" className={dot2} />
-        <span aria-hidden="true" className={dot3} />
+        <span className={dot} />
+        <span className={dot} />
+        <span className={dot} />
       </div>
     </div>
   );
