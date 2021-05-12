db7699999f2ddc515f8009123940925927af82a5
Sandesh <chapagainsandesh88@gmail.com>
2021-05-12 18:15:23 -0400

    accept placeholder image

--- src/design-system/containers/GridImage.js
+++ src/design-system/containers/GridImage.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function GridImage({ ...rest }) {
-  return <img className="w-full h-auto object-contain" {...rest} />;
-}
+const GridImage = React.memo(({ src, placeholder, alt = "" }) => {
+  const [loading, setLoading] = useState(true);
+  const [currentSrc, updateSrc] = useState(placeholder);
+
+  useEffect(() => {
+    const imageToLoad = new Image();
+    imageToLoad.src = src;
+    imageToLoad.onLoadStart = () => {
+      console.log("loading started for", src);
+    };
+    imageToLoad.onload = () => {
+      setLoading(false);
+      updateSrc(src);
+    };
+  }, [src]);
+
+  return (
+    <img
+      src={currentSrc}
+      style={{
+        opacity: loading ? 0.5 : 1,
+        transition: "opacity .15s linear",
+      }}
+      alt={alt}
+    />
+  );
+});
+
+export default GridImage;
