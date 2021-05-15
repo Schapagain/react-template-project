adc60e4272f7aa6c0e6255252df7c6793b36ec0c
Sandesh <chapagainsandesh88@gmail.com>
2021-05-15 18:07:27 -0400

    add util function to call API

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,3 +7,71 @@ const api = Axios.create({
   headers: { Accept: "application/json" },
 });
 export default api;
+
+/**
+ * Send bulk payload over the network
+ * @param {String} URL
+ * @param {Object} payload
+ * @param {String} token
+ * @param {String} method - HTTP method to use
+ * @param {String} contentType - form|json used to set headers
+ * @param {Function} onDownloadProgress - callback fired on download progress change
+ * @param {Function} onUploadProgress - callback fired on upload progress change
+ * @param {Function} onSuccess - callback fired after the action
+ * @param {Function} onUploadProgress - callback fired on upload progress change
+ */
+export async function callAPI({
+  url,
+  data,
+  token,
+  method = "post",
+  contentType = "json",
+  onUploadProgress = () => null,
+  onDownloadProgress = () => null,
+  onSuccess = () => null,
+  onError = () => null,
+}) {
+  console.log(method || "post", "ing to", url, data);
+  if (data.entries) {
+    for (var key of data.entries()) {
+      console.log(key[0] + " : " + key[1]);
+    }
+  }
+
+  const request = Axios.create({
+    baseURL: "",
+    headers: {
+      Authorization: `NPL ${token}`,
+      "Content-Type":
+        contentType === "json"
+          ? "application/json"
+          : contentType === "form"
+          ? "multipart/x-www-form-urlencoded"
+          : null,
+    },
+  });
+
+  try {
+    const result = await request.request({
+      method: method || "post",
+      data,
+      url,
+      onUploadProgress: (progressEvent) => {
+        let progress = Math.floor(
+          (progressEvent.loaded / progressEvent.total) * 100
+        );
+        onUploadProgress(progress);
+      },
+      onDownloadProgress: (progressEvent) => {
+        let progress = Math.floor(
+          (progressEvent.loaded / progressEvent.total) * 100
+        );
+        onDownloadProgress(progress);
+      },
+    });
+    onSuccess(result);
+    return result;
+  } catch (err) {
+    onError(err);
+  }
+}
